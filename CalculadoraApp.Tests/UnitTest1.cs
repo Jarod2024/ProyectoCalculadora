72b3999d26d9291f1272f1afaf229b5af69b9fec
Jarod2024 <jsguallichico1@espe.edu.ec>
2025-12-09 11:00:28 -0500

    Inicializa solución Calculadora con app y pruebas

---
+++ CalculadoraApp.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;    
+namespace CalculadoraApp.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Sumar_DosNumeros_VerificaSumaCorrecta()
+        {
+            // Arrange
+            var calc = new CalculadoraApp.calculadora();
+            var resultado = calc.Sumar(2, 3);
+            Assert.Equal(5, resultado); 
+
+
+        }
+    }
+}
